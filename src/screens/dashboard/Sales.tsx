7a10a5163604278328f3c76745cdeffe4529b68c
Farooq <farooqazammian@gmail.com>
2024-02-21 16:14:54 +0500

    Ui Improved

--- src/screens/dashboard/Sales.tsx
+++ src/screens/dashboard/Sales.tsx
@@ -16,66 +16,86 @@ const SegmentedButtonExample = () => {
         <View style={{ gap: 20 }} >
 
             <View style={{ flexDirection: "row", alignSelf: 'center' }}>
-                <Button mode={selectedSegment === 1 ? 'contained' : 'outlined'} onPress={() => setSelectedSegment(1)} style={{}}>
+                <Button
+                    mode={selectedSegment === 1 ? 'contained' : 'outlined'}
+                    onPress={() => setSelectedSegment(1)}
+                    color={selectedSegment === 1 ? '#468EFB' : 'black'}
+                    dark={selectedSegment === 1}
+                >
+
                     Months
-                </Button>
-                <Button mode={selectedSegment === 2 ? 'contained' : 'outlined'} onPress={() => setSelectedSegment(2)}  >
+                    </Button>
+                    <Button
+                    mode={selectedSegment === 2 ? 'contained' : 'outlined'}
+                    onPress={() => setSelectedSegment(2)}
+                    color={selectedSegment === 2 ? '#468EFB' : 'black'}
+                    dark={selectedSegment === 2}
+                >
                     Customers
                 </Button>
-                <Button mode={selectedSegment === 3 ? 'contained' : 'outlined'} onPress={() => setSelectedSegment(3)} >
+                <Button
+                    mode={selectedSegment === 3 ? 'contained' : 'outlined'}
+                    onPress={() => setSelectedSegment(3)}
+                    color={selectedSegment === 3 ? '#468EFB' : 'black'}
+                    dark={selectedSegment === 3}
+                >
                     Products
                 </Button>
             </View>
+
             <ScrollView contentContainerStyle={{ gap: 20 }}>
-                <TouchableRipple onPress={() => console.log("ripple pressed")} style={{}}>
-                    <View style={{ padding: 10, backgroundColor: "#fff", borderRadius: 10, width: "90%", alignSelf: 'center',gap:10 }} >
+                {selectedSegment === 2 && (
+                    <TouchableRipple onPress={() => console.log("ripple pressed")} style={{}}>
+                        <View style={{ padding: 10, backgroundColor: "#fff", borderRadius: 10, width: "90%", alignSelf: 'center', gap: 10 }} >
 
-                        <View style={{alignItems:'center'}}>
-                            <Text style={{ color: "#468EFB", fontSize: 25, fontWeight: "bold", }}>Sales by Customer</Text>
+                            <View style={{ alignItems: 'center' }}>
+                                <Text style={{ color: "#468EFB", fontSize: 25, fontWeight: "bold", }}>Sales by Customer</Text>
+                            </View>
+                            <View style={{ alignItems: 'center' }}>
+                                <PieChart
+                                    widthAndHeight={widthAndHeight}
+                                    series={series}
+                                    sliceColor={sliceColor}
+                                    coverRadius={0.5}
+                                    coverFill={'#FFF'}
+                                />
+                            </View>
+                            <View style={{ justifyContent: "flex-start", gap: 5 }}>
+                                <Text>Jerry</Text>
+                                <Text>Doe</Text>
+                                <Text>Tom</Text>
+                                <Text>Peter</Text>
+                            </View>
                         </View>
-                        <View style={{alignItems:'center'}}>
-                            <PieChart
-                                widthAndHeight={widthAndHeight}
-                                series={series}
-                                sliceColor={sliceColor}
-                                coverRadius={0.5}
-                                coverFill={'#FFF'}
-                            />
-                        </View>
-                        <View style={{ justifyContent: "flex-start" ,gap:5}}>
-                            <Text>Jerry</Text>
-                            <Text>Doe</Text>
-                            <Text>Tom</Text>
-                            <Text>Peter</Text>
-                        </View>
-                    </View>
+                    </TouchableRipple>
+                )}
 
-                </TouchableRipple>
+                {selectedSegment === 3 && (
+                    <TouchableRipple onPress={() => console.log("ripple pressed")} style={{}}>
+                        <View style={{ padding: 10, backgroundColor: "#fff", borderRadius: 10, width: "90%", alignSelf: 'center', gap: 10 }} >
 
-                <TouchableRipple onPress={() => console.log("ripple pressed")} style={{}}>
-                    <View style={{ padding: 10, backgroundColor: "#fff", borderRadius: 10, width: "90%", alignSelf: 'center',gap:10 }} >
+                            <View style={{ alignItems: 'center' }}>
+                                <Text style={{ color: "#468EFB", fontSize: 25, fontWeight: "bold", }}>Sales by Product</Text>
+                            </View>
+                            <View style={{ alignItems: 'center' }}>
+                                <PieChart
+                                    widthAndHeight={widthAndHeight}
+                                    series={series}
+                                    sliceColor={sliceColor}
+                                    coverRadius={0.5}
+                                    coverFill={'#FFF'}
+                                />
+                            </View>
+                            <View style={{ justifyContent: "flex-start", gap: 5 }}>
+                                <Text>Cos</Text>
+                                <Text>Doe</Text>
+                                <Text>Tom</Text>
+                                <Text>Peter</Text>
+                            </View>
+                        </View>
 
-                        <View style={{alignItems:'center'}}>
-                            <Text style={{ color: "#468EFB", fontSize: 25, fontWeight: "bold", }}>Sales by Products</Text>
-                        </View>
-                        <View style={{alignItems:'center'}}>
-                            <PieChart
-                                widthAndHeight={widthAndHeight}
-                                series={series}
-                                sliceColor={sliceColor}
-                                coverRadius={0.5}
-                                coverFill={'#FFF'}
-                            />
-                        </View>
-                        <View style={{ justifyContent: "flex-start" ,gap:5}}>
-                            <Text>Cos</Text>
-                            <Text>Doe</Text>
-                            <Text>Tom</Text>
-                            <Text>Peter</Text>
-                        </View>
-                    </View>
-
-                </TouchableRipple>
+                    </TouchableRipple>
+                )}
             </ScrollView>
         </View>
     );
@@ -90,3 +110,49 @@ const styles = StyleSheet.create({
 })
 
 export default SegmentedButtonExample;
+
+// <View style={{ gap:  20 }}>
+// <View style={{ flexDirection: "row", alignSelf: 'center' }}>
+//     <Button mode={selectedSegment ===  1 ? 'contained' : 'outlined'} onPress={() => setSelectedSegment(1)} style={{}}>
+//         Months
+//     </Button>
+//     <Button mode={selectedSegment ===  2 ? 'contained' : 'outlined'} onPress={() => setSelectedSegment(2)}>
+//         Customers
+//     </Button>
+//     <Button mode={selectedSegment ===  3 ? 'contained' : 'outlined'} onPress={() => setSelectedSegment(3)}>
+//         Products
+//     </Button>
+// </View>
+// <ScrollView contentContainerStyle={{ gap:  20 }}>
+//     {selectedSegment ===  2 && (
+//         <TouchableRipple onPress={() => console.log("ripple pressed")} style={{ width: "80%", alignSelf: 'center' }}>
+//             <View style={{ padding:  10, backgroundColor: "#fff", borderRadius:  10, alignSelf: 'center' }}>
+//                 {/* ... */}
+//                 <PieChart
+//                     widthAndHeight={widthAndHeight}
+//                     series={series}
+//                     sliceColor={sliceColor}
+//                     coverRadius={0.5}
+//                     coverFill={'#FFF'}
+//                 />
+//                 {/* ... */}
+//             </View>
+//         </TouchableRipple>
+//     )}
+//     {selectedSegment ===  3 && (
+//         <TouchableRipple onPress={() => console.log("ripple pressed")} style={{ width: "80%", alignSelf: 'center' }}>
+//             <View style={{ padding:  10, backgroundColor: "#fff", borderRadius:  10, alignSelf: 'center' }}>
+//                 {/* ... */}
+//                 <PieChart
+//                     widthAndHeight={widthAndHeight}
+//                     series={series}
+//                     sliceColor={sliceColor}
+//                     coverRadius={0.5}
+//                     coverFill={'#FFF'}
+//                 />
+//                 {/* ... */}
+//             </View>
+//         </TouchableRipple>
+//     )}
+// </ScrollView>
+// </View>
