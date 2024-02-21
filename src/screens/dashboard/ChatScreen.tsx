712cb68bba3abee201adfa21dd185b68bb47e953
Farooq <farooqazammian@gmail.com>
2024-02-21 17:56:02 +0500

    Sales UI Improved
    -Pie Graph improved

--- src/screens/dashboard/ChatScreen.tsx
+++ src/screens/dashboard/ChatScreen.tsx
@@ -1,137 +1,36 @@
-import { AntDesign, Feather } from '@expo/vector-icons';
-import React, { useState } from 'react';
-import { View, Dimensions, StyleSheet, ScrollView } from 'react-native';
-import { Button, Text, TouchableRipple } from 'react-native-paper';
-import PieChart from 'react-native-pie-chart'
+import React from 'react';
+import { View } from 'react-native';
+import { BarChart } from 'react-native-gifted-charts';
 
-
-const { width } = Dimensions.get('window')
 const ChatScreen = () => {
-  const [selectedSegment, setSelectedSegment] = useState(0);
-  const [selectedCustomer, setSelectedCustomer] = useState(null);
-  
-  const widthAndHeight = 200
-  const salesData = {
-    Jerry: 123,
-    Doe: 521,
-    Tom: 233,
-    Peter: 789,
-    Emilia: 537,
-  };
+  // Sample data for the chart
+  const data = [
+    { label: 'Jan', value:  100 },
+    { label: 'Feb', value:  150 },
+    { label: 'Mar', value:  200 },
+    // ... add more data points as needed
+  ];
 
-
-  const sliceColors = ['#fbd203', '#7F27FF', '#2ecc71', '#ff3c00', '#468EFB',]
-  const series = Object.values(salesData);
-  const sliceColor = Object.keys(salesData).map((customer, index) => {
-    return customer === selectedCustomer ? '#000' : sliceColors[index];
-  });
-
-  const totalSales = Object.values(salesData).reduce((a, b) => a + b, 0);
-
-  
-  const customerPercentages = Object.entries(salesData).map(([customer, sales]) => {
-    return { customer, percentage: (sales / totalSales) * 100 };
-  });
   return (
-
-    <View style={{ gap: 20 }} >
-
-      <View style={{ flexDirection: "row", alignSelf: 'center' }}>
-
-        <Button
-          mode={selectedSegment === 1 ? 'contained' : 'outlined'}
-          onPress={() => setSelectedSegment(1)}
-          color={selectedSegment === 1 ? '#468EFB' : 'black'}
-          dark={selectedSegment === 1}
-        >
-          Customers
-        </Button>
-        <Button
-          mode={selectedSegment === 2 ? 'contained' : 'outlined'}
-          onPress={() => setSelectedSegment(2)}
-          color={selectedSegment === 2 ? '#468EFB' : 'black'}
-          dark={selectedSegment === 2}
-        >
-          Products
-        </Button>
-      </View>
-
-      <ScrollView contentContainerStyle={{ gap: 20, }}>
-        {selectedSegment === 1 && (
-
-          <View style={{ padding: 10, backgroundColor: "#fff", borderRadius: 10, width: "90%", alignSelf: 'center', gap: 10, }} >
-
-            <View style={{ alignItems: 'center' }}>
-              <Text style={{ color: "#468EFB", fontSize: 25, fontWeight: "bold", }}>Sales by Customer</Text>
-            </View>
-            <View style={{ alignItems: 'center' }}>
-              <PieChart
-                widthAndHeight={widthAndHeight}
-                series={series}
-                sliceColor={sliceColor}
-                coverRadius={0.5}
-                coverFill={'#FFF'}
-              />
-            </View>
-            <View  style={{flexDirection:"row", gap:20,}}>
-              <View style={{gap:5,}}>
-              {customerPercentages.map(({ customer }) => (
-                <Text key={customer} onPress={() => setSelectedCustomer(customer)} style={{fontSize:20,}}>
-                  {customer} 
-                </Text>
-              ))}
-              </View>
-              <View style={{gap:5}}>
-              {customerPercentages.map(({  percentage }) => (
-                <Text key={percentage} onPress={() => setSelectedCustomer(percentage)} style={{fontSize:20,}}>
-                  {percentage.toFixed(2)} %
-                </Text>
-              ))}
-              </View>
-            </View>
-          </View>
-
-        )}
-
-        {selectedSegment === 2 && (
-          <TouchableRipple onPress={() => console.log("ripple pressed")} style={{}}>
-            <View style={{ padding: 10, backgroundColor: "#fff", borderRadius: 10, width: "90%", alignSelf: 'center', gap: 10 }} >
-
-              <View style={{ alignItems: 'center' }}>
-                <Text style={{ color: "#468EFB", fontSize: 25, fontWeight: "bold", }}>Sales by Product</Text>
-              </View>
-              <View style={{ alignItems: 'center' }}>
-                <PieChart
-                  widthAndHeight={widthAndHeight}
-                  series={series}
-                  sliceColor={sliceColor}
-                  coverRadius={0.5}
-                  coverFill={'#FFF'}
-                />
-              </View>
-              <View style={{ justifyContent: "flex-start", gap: 5 }}>
-                <Text>Cosflor</Text>
-                <Text>Refix</Text>
-                <Text>Augmen</Text>
-                <Text>Costio</Text>
-                <Text>Mativ</Text>
-              </View>
-            </View>
-
-          </TouchableRipple>
-        )}
-      </ScrollView>
+    <View style={{ flex:  1, justifyContent: 'center', alignItems: 'center' }}>
+      <BarChart
+        data={data}
+        width={300}
+        height={200}
+        chartConfig={{
+          backgroundColor: '#e26a00',
+          backgroundGradientFrom: '#fb8c00',
+          backgroundGradientTo: '#ffa726',
+          color: (opacity =  1) => `rgba(255,  255,  255, ${opacity})`,
+          labelColor: (opacity =  1) => `rgba(255,  255,  255, ${opacity})`,
+        }}
+        style={{
+          marginVertical:  8,
+          borderRadius:  16,
+        }}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-
-  title: {
-    fontSize: 24,
-    margin: 10,
-  },
-})
-
 export default ChatScreen;
-
--- src/screens/dashboard/Sales.tsx
+++ src/screens/dashboard/Sales.tsx
@@ -3,14 +3,51 @@ import React, { useState } from 'react';
 import { View, Dimensions, StyleSheet, ScrollView } from 'react-native';
 import { Button, Text, TouchableRipple } from 'react-native-paper';
 import PieChart from 'react-native-pie-chart'
+import DividerBar from '../../components/Divider/DividerBar';
 
 
 const { width } = Dimensions.get('window')
 const Sales = () => {
     const [selectedSegment, setSelectedSegment] = useState(0);
+    const [selectedCustomer, setSelectedCustomer] = useState(null);
+    const [selectedProduct, setSelectedProduct] = useState(null)
     const widthAndHeight = 200
-    const series = [123, 521, 123, 789, 537,]
-    const sliceColor = ['#fbd203', '#000', '#468EFB', '#2ecc71', '#ff3c00']
+    const customerSalesData = {
+        Jerry: 123,
+        Doe: 521,
+        Tom: 433,
+        Peter: 789,
+        Emilia: 537,
+    };
+    const productSalesData = {
+        Cosflor: 323,
+        Regix: 421,
+        Costio: 133,
+        Refix: 589,
+        Mativ: 437,
+    };
+
+    const sliceColors = ['#fbd203', '#7F27FF', '#2ecc71', '#ff3c00', '#468EFB',]
+
+    const customerSeries = Object.values(customerSalesData);
+    const sliceCustomerColor = Object.keys(customerSalesData).map((customer, index) => {
+        return customer === selectedCustomer ? '#000' : sliceColors[index];
+    });
+
+    const productSeries = Object.values(productSalesData);
+    const sliceProductColor = Object.keys(productSalesData).map((product, index) => {
+        return product === selectedProduct ? '#000' : sliceColors[index];
+    });
+
+    const totalSales = Object.values(customerSalesData).reduce((a, b) => a + b, 0);
+
+
+    const customerPercentages = Object.entries(customerSalesData).map(([customer, sales]) => {
+        return { customer, percentage: (sales / totalSales) * 100 };
+    });
+    const productPercentages = Object.entries(productSalesData).map(([product, sales]) => {
+        return { product, percentage: (sales / totalSales) * 100 };
+    });
     return (
 
         <View style={{ gap: 20 }} >
@@ -24,8 +61,8 @@ const Sales = () => {
                 >
 
                     Months
-                    </Button>
-                    <Button
+                </Button>
+                <Button
                     mode={selectedSegment === 2 ? 'contained' : 'outlined'}
                     onPress={() => setSelectedSegment(2)}
                     color={selectedSegment === 2 ? '#468EFB' : 'black'}
@@ -45,56 +82,82 @@ const Sales = () => {
 
             <ScrollView contentContainerStyle={{ gap: 20, }}>
                 {selectedSegment === 2 && (
-                    <TouchableRipple onPress={() => console.log("ripple pressed")} style={{}}>
-                        <View style={{ padding: 10, backgroundColor: "#fff", borderRadius: 10, width: "90%", alignSelf: 'center', gap: 10, }} >
 
-                            <View style={{ alignItems: 'center' }}>
-                                <Text style={{ color: "#468EFB", fontSize: 25, fontWeight: "bold", }}>Sales by Customer</Text>
+                    <View style={{ padding: 10, backgroundColor: "#fff", borderRadius: 10, width: "90%", alignSelf: 'center', gap: 10, }} >
+
+                        <View style={{ alignItems: 'center' }}>
+                            <Text style={{ color: "#468EFB", fontSize: 25, fontWeight: "bold", }}>Sales by Customer</Text>
+                        </View>
+                        <View style={{ alignItems: 'center' }}>
+                            <PieChart
+                                widthAndHeight={widthAndHeight}
+                                series={customerSeries}
+                                sliceColor={sliceCustomerColor}
+                                coverRadius={0.5}
+                                coverFill={'#FFF'}
+                            />
+                        </View>
+                        <View style={{ flexDirection: "row", gap: 20, }}>
+                            <View style={{ gap: 5, }}>
+                                
+                                {customerPercentages.map(({ customer }) => (
+                                    <Text key={customer} onPress={() => setSelectedCustomer(customer)} style={{ fontSize: 20, }}>
+                                        {customer}
+
+                                    </Text>
+                                ))}
+
                             </View>
-                            <View style={{ alignItems: 'center' }}>
-                                <PieChart
-                                    widthAndHeight={widthAndHeight}
-                                    series={series}
-                                    sliceColor={sliceColor}
-                                    coverRadius={0.5}
-                                    coverFill={'#FFF'}
-                                />
-                            </View>
-                            <View style={{ justifyContent: "flex-start", gap: 5 }}>
-                                <Text>Jerry</Text>
-                                <Text>Doe</Text>
-                                <Text>Tom</Text>
-                                <Text>Peter</Text>
+
+                            <View style={{ gap: 5 }}>
+                                {customerPercentages.map(({ percentage }) => (
+                                    <Text key={percentage} style={{ fontSize: 20, }}>
+                                        {percentage.toFixed(2)} %
+                                    </Text>
+
+                                ))}
+
                             </View>
                         </View>
-                    </TouchableRipple>
+                    </View>
+
                 )}
 
-                {selectedSegment === 3 && (
-                    <TouchableRipple onPress={() => console.log("ripple pressed")} style={{}}>
-                        <View style={{ padding: 10, backgroundColor: "#fff", borderRadius: 10, width: "90%", alignSelf: 'center', gap: 10 }} >
 
-                            <View style={{ alignItems: 'center' }}>
-                                <Text style={{ color: "#468EFB", fontSize: 25, fontWeight: "bold", }}>Sales by Product</Text>
+                {selectedSegment === 3 && (
+
+                    <View style={{ padding: 10, backgroundColor: "#fff", borderRadius: 10, width: "90%", alignSelf: 'center', gap: 10, }} >
+
+                        <View style={{ alignItems: 'center' }}>
+                            <Text style={{ color: "#468EFB", fontSize: 25, fontWeight: "bold", }}>Sales by Products</Text>
+                        </View>
+                        <View style={{ alignItems: 'center' }}>
+                            <PieChart
+                                widthAndHeight={widthAndHeight}
+                                series={productSeries}
+                                sliceColor={sliceProductColor}
+                                coverRadius={0.5}
+                                coverFill={'#FFF'}
+                            />
+                        </View>
+                        <View style={{ flexDirection: "row", gap: 20, }}>
+                            <View style={{ gap: 5, }}>
+                            {productPercentages.map(({ product }) => (
+                                    <Text key={product} onPress={() => setSelectedProduct(product)} style={{ fontSize: 20, }}>
+                                        {product}
+                                    </Text>
+                                ))}
                             </View>
-                            <View style={{ alignItems: 'center' }}>
-                                <PieChart
-                                    widthAndHeight={widthAndHeight}
-                                    series={series}
-                                    sliceColor={sliceColor}
-                                    coverRadius={0.5}
-                                    coverFill={'#FFF'}
-                                />
-                            </View>
-                            <View style={{ justifyContent: "flex-start", gap: 5 }}>
-                                <Text>Cos</Text>
-                                <Text>Doe</Text>
-                                <Text>Tom</Text>
-                                <Text>Peter</Text>
+                            <View style={{ gap: 5 }}>
+                                {productPercentages.map(({ percentage }) => (
+                                    <Text key={percentage} onPress={() => setSelectedProduct(percentage)} style={{ fontSize: 20, }}>
+                                        {percentage.toFixed(2)} %
+                                    </Text>
+                                ))}
                             </View>
                         </View>
+                    </View>
 
-                    </TouchableRipple>
                 )}
             </ScrollView>
         </View>
