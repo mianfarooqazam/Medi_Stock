ed4b350ceee5951d8fdd760f6ec1ce016be36077
Farooq <farooqazammian@gmail.com>
2024-02-26 18:57:33 +0500

    Ui Changed

--- src/screens/dashboard/StockInOut.tsx
+++ src/screens/dashboard/StockInOut.tsx
@@ -5,46 +5,35 @@ const StockInOut = () => {
   const currentDate = moment().format("DD-MM-YYYY");
   return (
     <View style={styles.container}>
-      
+       {/* <Text style={[styles.tableData,{fontSize:10}]}>{currentDate.split('-')[0] + '-' + currentDate.split('-')[1] + '\n' + currentDate.split('-')[2]}</Text> */}
        <View>
       <View style={styles.table}>
         <Text style={styles.tableHeader}>S/N</Text>
-        <Text style={styles.tableHeader}>Date</Text>
-        <Text style={styles.tableHeader}>Packing</Text>
-        <Text style={styles.tableHeader}>Product Name</Text>
-        
-        <Text style={styles.tableHeader}>⬆️</Text>
-        <Text style={styles.tableHeader}>⬇️</Text>
+        <Text style={styles.tableHeader}>Product</Text>
+        <Text style={styles.tableHeader}>Stock-In</Text>
+        <Text style={styles.tableHeader}>Stock-Out</Text>
       </View>
 
       {/* t-row */}
       <View style={styles.tableRow}>
         <Text style={styles.tableData}>1</Text>
-        <Text style={[styles.tableData,{fontSize:10}]}>{currentDate.split('-')[0] + '-' + currentDate.split('-')[1] + '\n' + currentDate.split('-')[2]}</Text>
-        <Text style={styles.tableData}>Sach</Text>
-        <Text style={styles.tableData}>Cosflor</Text>
-        
-        <Text style={styles.tableData}>21</Text>
-        <Text style={styles.tableData}>21</Text>
+        <Text style={styles.tableData}>Cosflor</Text>   
+        <Text style={styles.tableData}>232   <Text style={[styles.tableData,{fontSize:10,color:"#bebebe"}]}>{currentDate}</Text> </Text>
+        <Text style={styles.tableData}>232   <Text style={[styles.tableData,{fontSize:10,color:"#bebebe"}]}>{currentDate}</Text> </Text>
       </View>
+
       <View style={styles.tableRow}>
         <Text style={styles.tableData}>2</Text>
-        <Text style={[styles.tableData,{fontSize:10}]}>{currentDate.split('-')[0] + '-' + currentDate.split('-')[1] + '\n' + currentDate.split('-')[2]}</Text>
-        <Text style={styles.tableData}>Syp</Text>
         <Text style={styles.tableData}>Inicos</Text>
-        
-        <Text style={styles.tableData}>20</Text>
-        <Text style={styles.tableData}>21</Text>
+        <Text style={styles.tableData}>232   <Text style={[styles.tableData,{fontSize:10,color:"#bebebe"}]}>{currentDate}</Text> </Text>
+        <Text style={styles.tableData}>232   <Text style={[styles.tableData,{fontSize:10,color:"#bebebe"}]}>{currentDate}</Text> </Text>
       </View>
 
       <View style={styles.tableRow}>
         <Text style={styles.tableData}>3</Text>
-        <Text style={[styles.tableData,{fontSize:10}]}>{currentDate.split('-')[0] + '-' + currentDate.split('-')[1] + '\n' + currentDate.split('-')[2]}</Text>
-        <Text style={styles.tableData}>Cap</Text>
         <Text style={styles.tableData}>Refix</Text>
-        
-        <Text style={styles.tableData}>7</Text>
-        <Text style={styles.tableData}>21</Text>
+        <Text style={styles.tableData}>232   <Text style={[styles.tableData,{fontSize:10,color:"#bebebe"}]}>{currentDate}</Text> </Text>
+        <Text style={styles.tableData}>232   <Text style={[styles.tableData,{fontSize:10,color:"#bebebe"}]}>{currentDate}</Text> </Text>
       </View>
      </View>
      
@@ -85,12 +74,17 @@ tableRow: {
   flexDirection: 'row',
   borderBottomWidth: 1,
   borderBottomColor: '#ddd',
-  padding:5
+  // padding:5,
+  alignItems: 'center',
 },
 tableData: {
   flex: 1,
   padding: 10,
   textAlign: 'center',
+},
+subText: {
+  fontSize: 10,
+  color: "#bebebe",
 },
 })
 
