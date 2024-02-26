7eaea335465f96733a3dacae3aebfdc20eae8f22
Farooq <farooqazammian@gmail.com>
2024-02-26 21:59:50 +0500

    Update Stock In-Out

--- src/screens/dashboard/StockInOut.tsx
+++ src/screens/dashboard/StockInOut.tsx
@@ -2,11 +2,31 @@ import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { getDocs, collection, query, where, orderBy } from 'firebase/firestore';
 import firebase from '../../../firebaseConfig';
-import { Searchbar } from 'react-native-paper';
+import { Modal, Portal, Searchbar, TextInput } from 'react-native-paper';
+import { Feather } from '@expo/vector-icons';
+import Toast from 'react-native-toast-message';
+import ReusableButton from '../../components/Button/ReusableButton';
+import DividerBar from '../../components/Divider/DividerBar';
+import { Controller, useForm } from 'react-hook-form';
+import { Dropdown } from 'react-native-element-dropdown';
+import ResuableInput from '../../components/TextInput/ReusableInput';
+
+const containerStyle = {
+  backgroundColor: "white",
+  padding: 20,
+  width: "80%",
+  alignSelf: "center",
+  justifyContent: "center",
+  borderRadius: 10,
+};
 
 const StockInOut = () => {
+  const [newStock, setNewStock] = useState(false);
+  const showNewStockModal = () => setNewStock(true);
+  const hideNewStockModal = () => setNewStock(false);
   const [stocks, setStocks] = useState([]);
   const [searchQuery, setSearchQuery] = useState("")
+  const { control, handleSubmit } = useForm();
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -29,6 +49,14 @@ const StockInOut = () => {
 
     fetchData();
   }, [searchQuery]);
+  const addMessage = () => {
+    Toast.show({
+      type: 'success',
+      position: 'top',
+      text1: 'Success',
+      text2: 'Customer Added',
+    });
+  }
   return (
     <View style={styles.container}>
       <Searchbar onChangeText={setSearchQuery}
@@ -52,20 +80,79 @@ const StockInOut = () => {
             </View>
           ) : (
             stocks.map((stocks, index) => (
-              
-      <View style={styles.tableRow} key={index}>
-        <Text style={styles.tableData}>{stocks.ProductName}</Text>   
-        <Text style={[styles.tableData, {color:"#008000"}]}>{stocks.StockIn} </Text>
-        <Text style={[styles.tableData, {color:"#FF0000"}]}>{stocks.StockOut} </Text>
-      </View>
 
- 
+              <View style={styles.tableRow} key={index}>
+                <Text style={styles.tableData}>{stocks.ProductName}</Text>
+                <Text style={[styles.tableData, { color: "#008000" }]}>{stocks.StockIn} </Text>
+                <Text style={[styles.tableData, { color: "#FF0000" }]}>{stocks.StockOut} </Text>
+              </View>
             )
             ))}
 
         </ScrollView>
-      </View>
 
+      </View>
+      <View style={styles.iconContainer} >
+        <Feather name="plus" size={40} color="#fff" onPress={() => showNewStockModal()} />
+      </View>
+      <Portal>
+        <Modal
+          visible={newStock}
+          onDismiss={hideNewStockModal}
+          contentContainerStyle={containerStyle}
+        >
+          <View style={{ gap: 10 }}>
+            <Text style={{ fontWeight: "bold" }}>Add Stock</Text>
+            <DividerBar />
+
+            <Controller
+              control={control}
+              render={({ field: { onChange, value, onBlur } }) => (
+                <Dropdown
+                  style={styles.dropdown}
+                  placeholderStyle={styles.placeholderStyle}
+                  selectedTextStyle={styles.selectedTextStyle}
+                  inputSearchStyle={styles.inputSearchStyle}
+                  iconStyle={styles.iconStyle}
+                  data={[]}
+                  search
+                  maxHeight={300}
+                  labelField="customerName"
+                  valueField="customerName"
+                  placeholder="Select Product"
+                  searchPlaceholder="Search Product"
+                  value={value}
+                  onBlur={onBlur}
+                  onChange={onChange} />
+              )}
+              name="stock"
+            />
+            <Controller
+              control={control}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <ResuableInput
+                  mode='outlined'
+                  label="Stock-In"
+                  onBlur={onBlur}
+                  onChangeText={value => onChange(value)}
+                  value={value}
+                  style={styles.textInput}
+                  activeOutlineColor='#4683FB'
+                  outlineColor='#4683FB'
+                />
+              )}
+              name="stockin"
+              rules={{ required: true }}
+              defaultValue=""
+            />
+           
+
+            <View style={{}}>
+              <ReusableButton label="Update Stock" onPress={() => { }} style={styles.button} textColor="#fff" />
+            </View>
+          </View>
+        </Modal>
+      </Portal>
     </View>
   )
 }
@@ -73,8 +160,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    // marginVertical:10,
-    gap:10
+    gap: 10,
   }, heading: {
     fontSize: 20,
     // fontWeight: 'bold',
@@ -123,7 +209,55 @@ const styles = StyleSheet.create({
   },
   noResultsText: {
     fontSize: 20,
-    color:"#468EFB"
+    color: "#468EFB"
+  },
+  iconContainer: {
+    position: 'absolute',
+    right: 50,
+    bottom: 50,
+    // borderWidth:1,
+    backgroundColor: "#4683fb",
+    borderRadius: 10,
+    padding: 5
+  },
+  // },
+  dropdown: {
+    height: 50,
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingHorizontal: 8,
+    width: "100%",
+    alignSelf: "center",
+  },
+  placeholderStyle: {
+    fontSize: 16,
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+  },
+  iconStyle: {
+    width: 20,
+    height: 20,
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 16,
+  },
+  textInput: {
+    backgroundColor: "#fff",
+    width: "100%",
+    alignSelf: 'center',
+  },
+  button: {
+    backgroundColor: "#4683fb",
+    width: "100%",
+    alignSelf: "center",
+  },
+  datePicker: {
+    width: '100%',
+    marginTop: 10,
+    marginBottom: 10,
   },
 })
 
